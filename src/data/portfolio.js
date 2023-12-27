2da811fb1e35184e7c5a59405effc52ff58b9eaa
Renaud2002 <rrenaudnoubieptie@gmail.com>
2023-12-26 20:29:45 -0500

    deployment work

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -1,19 +1,19 @@
 export default[
     {
         title: 'Reaction Wheel',
-        imgUrl: '/renaud-site/assets/reaction_wheel.jpg',
+        imgUrl: '/assets/reaction_wheel.jpg',
         stack: ['Arduino Nano', 'C++', 'PWM Nidec Motor'],
         link: 'google.com'
     },
     {
         title: 'This Website',
-        imgUrl: '/renaud-site/assets/dev_site.jpg',
+        imgUrl: '/assets/dev_site.jpg',
         stack: ['React', 'Tailwind', 'Vite', 'ForrestKnight Youtube'],
         link: 'google.com'
     },
     {
         title: 'Comming Soon ...',
-        imgUrl: '/renaud-site/assets/coming_soon.jpeg',
+        imgUrl: '/assets/coming_soon.jpeg',
         stack: ['Working'],
         link: 'google.com'
     }
